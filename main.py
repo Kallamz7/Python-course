641c17fb9e2e2a9ef029e6bd62276a77720ec0f0
Kallamz <klassicstar19@gmail.com>
2022-10-15 20:18:37 +0100

    Git is nice

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 print(f'Volume of a Cone')
 pi = 3.142
-r = int(input(f'Enter your radius'/n))
-h = int(input(f'Enter your height'))
+r = int(input(f'Enter your radius'))
+h = int(input(f'\nEnter your height'))
 
 v = pi * pow(r, 2) * h/3
 
-print(f'Your volume is {v}')
+print(f'\nYour volume is {v}')
